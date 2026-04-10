9321cfc15696bd26b6b9a5c613918833fc82ad6d
Michael Schneider <michael.schneider@envoyb2b.com>
2026-04-10 16:49:19 -0400

    Visual scrollbar

--- init.el
+++ init.el
@@ -43,6 +43,10 @@
 ; ====================
 ; Package installs
 ; ====================
+(use-package yascroll
+  :ensure t
+  :config
+  (global-yascroll-bar-mode 1))
 (use-package vertico
   :ensure t
   :init
@@ -807,11 +811,7 @@ Reselects duplicated region under Spacemacs holy mode."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(breadcrumb consult dirvish doom-modeline doom-themes
-                exec-path-from-shell jq-mode magit marginalia
-                move-text multiple-cursors nushell-mode orderless
-                projectile treesit-auto vertico yasnippet-snippets))
+ '(package-selected-packages nil)
  '(safe-local-variable-directories '("/Users/michaelschneider/appropos/envoy-web/")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
